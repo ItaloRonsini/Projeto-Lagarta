77dd5de7dfa3941d89f578d3ef4716887d16f0a1
gabriel.alonso <gabriel.bijalon@gmail.com>
2026-09-01 09:32:09 -0300

    Update guia-alimentacao.js

--- guia-alimentacao.js
+++ guia-alimentacao.js
@@ -1,1 +1,29 @@
+function mostrarDica(categoria) {
+    let mensagem = "";
+
+    if (categoria === "frutas") {
+        mensagem = "As frutas possuem vitaminas, fibras e minerais importantes para o crescimento.";
+    }
+
+    else if (categoria === "vegetais") {
+        mensagem = "Os vegetais ajudam na imunidade e no funcionamento do organismo.";
+    }
+
+    else if (categoria === "proteinas") {
+        mensagem = "As proteínas ajudam no crescimento e na formação dos músculos.";
+    }
+
+    else if (categoria === "graos") {
+        mensagem = "Os grãos fornecem energia para estudar, brincar e praticar atividades.";
+    }
+
+    else if (categoria === "laticinios") {
+        mensagem = "Leite e derivados são importantes fontes de cálcio.";
+    }
+
+    else if (categoria === "agua") {
+        mensagem = "Beber água durante o dia ajuda a manter o corpo hidratado.";
+    }
+
+    document.getElementById(categoria).innerHTML = mensagem;
 
